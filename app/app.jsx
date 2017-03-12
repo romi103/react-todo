8ee252286c0b41c5ba61ae2706aee919c7564078
Roman <romaan.lorent@gmail.com>
2017-03-12 20:01:36 +0000

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory } = require('react-router');
+
+
+// Load foundation
+
+$(document).foundation();
+
+// app css
+require('style!css!sass!applicationStyles');
+
+
+ReactDOM.render(
+   <p>Boilerplate3</p>,
+    document.getElementById("app")
+    );
+    
