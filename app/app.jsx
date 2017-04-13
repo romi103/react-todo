5b8d09ef465ca01a3bbf7d6ade4bf08a5435f331
Roman <romaan.lorent@gmail.com>
2017-04-13 22:08:47 +0100

    TodoApp component created

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory } = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 
 // Load foundation
 
@@ -12,7 +14,7 @@ require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
-   <p>Boilerplate3</p>,
+   <TodoApp/>,
     document.getElementById("app")
     );
     
